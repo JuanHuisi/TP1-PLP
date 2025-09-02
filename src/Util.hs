a5c99c842bb6f6e260ce9ec0b1f2849d76ee1d34
JuanHuisi <jcromero@dc.uba.ar>
2025-09-02 18:58:50 -0300

    Implement alinearDerecha and actualizarElem functions in Util module

--- src/Util.hs
+++ src/Util.hs
@@ -1,16 +1,21 @@
 module Util where
 
+
 -- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
 -- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
 alinearDerecha :: Int -> String -> String
-alinearDerecha n s = error "COMPLETAR EJERCICIO 1"
+alinearDerecha n s = if length s >= n then s 
+                                        else take (n - length s) (repeat ' ') ++ s
 
 -- | Dado un índice y una función, actualiza el elemento en la posición del índice
 -- aplicando la función al valor actual. Si el índice está fuera de los límites
 -- de la lista, devuelve la lista sin cambios.
 -- El primer elemento de la lista es el índice 0.
 actualizarElem :: Int -> (a -> a) -> [a] -> [a]
-actualizarElem n f xs = error "COMPLETAR EJERCICIO 2"
+actualizarElem n f xs = actualizarElem' f xs n
+
+actualizarElem' :: (a -> a) -> [a] -> Int -> [a]
+actualizarElem' f = foldr (\x rec i -> if i == 0 then f x : rec (i-1) else x : rec (i-1)) (const [])
 
 -- | infinito positivo (Haskell no tiene literal para +infinito)
 infinitoPositivo :: Float
